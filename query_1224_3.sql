ee676017e7f4c808cf17e87a5e184830d3b6e188
freetic <freetic1004@naver.com>
2019-12-24 16:18:11 +0900

    a

--- query_1224_3.sql
+++ query_1224_3.sql
@@ -1,15 +1,26 @@
 ------- outer join : 한 쪽 테이블에만 레코드가 존재해도 조회.
 -- 모든 부서의 부서 번호, 부서명, 위치, 사원번호, 사원명, 입사일 조회
 -- 단, 사원이 존재하지 않는 부서는 사원번호 - 0, 사원명 - anony, 입사일 - 오늘로 넣어 출력
+-- ANSI 문법
 select d.deptno, d.dname, d.loc, nvl(e.empno, 0) empno, nvl(e.ename, 'anony') ename, nvl(e.hiredate, sysdate) hiredate
 from dept d left join emp e
 on e.deptno = d.deptno;
+-- Oracle 문법
+select d.deptno, d.dname, d.loc, nvl(e.empno, 0) empno, nvl(e.ename, 'anony') ename, nvl(e.hiredate, sysdate) hiredate
+from dept d,emp e
+where e.deptno (+)= d.deptno;
 
 -- select 제조사가 BMW, 쌍용, 기아인 모든 차량의 제조국, 제조사, 모델명, 가격, 연식을 조회하라.
+-- ANSI 문법
 select cc.country, cc.maker, cmo.model, cmo.price, cmo.car_year
-from car_country cc, car_maker cma left join car_model cmo
+from car_country cc, car_maker cma left outer join car_model cmo
 on cma.model = cmo.model
 where cc.maker = cma.maker and cma.maker in('기아','쌍용','BMW');
+
+-- Oracle 문법
+select cc.country, cc.maker, cmo.model, cmo.price, cmo.car_year
+from car_country cc, car_maker cma, car_model cmo
+where ( cc.maker = cma.maker and cmo.model (+)= cma.model ) and cma.maker in('기아','쌍용','BMW');
 
 select * from car_maker;
 select * from car_model;
