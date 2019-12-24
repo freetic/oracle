3cf66209393f143c036192e1299faa4cf581e9fb
freetic <freetic1004@naver.com>
2019-12-24 16:07:08 +0900

    a

--- query_1224_2.sql
+++ query_1224_2.sql
@@ -42,4 +42,56 @@ inner join dept d
 on e.deptno=d.deptno;
 
 -- Oracle inner join
+select d.deptno, d.dname, d.loc, e.empno, e.ename, e.sal, e.hiredate, e.job
+from emp e, dept d
+where e.deptno=d.deptno;
 
+select * from car_country;
+select * from car_maker;
+select * from car_model;
+
+
+-- 제조국이 '국산'인 모든 제조사의 제조국, 제조사, 모델명 조회
+select cc.country, cc.maker, cma.model
+from car_country cc
+inner join car_maker cma
+on cc.maker = cma.maker
+where country = '국산';
+
+select cc.country, cc.maker, cma.model
+from car_country cc, car_maker cma
+where ( cc.maker = cma.maker ) and country = '국산';
+
+-- 제조사가 현대인 차량의 제조국, 제조사, 모델명, 연식, 가격, 옵션을 조회
+-- 단, 보유중인 차량이 존재하는 경우만 조회
+-- ANSI
+select cc.country, cc.maker, cma.model, cmo.car_year, cmo.price, cmo.car_option
+from car_country cc
+inner join car_maker cma
+on cma.maker = cc.maker
+inner join car_model cmo
+on cmo.model = cma.model
+where cc.maker = '현대';
+-- Oracle
+select cc.country, cc.maker, cma.model, cmo.car_year, cmo.price, cmo.car_option
+from car_maker cma, car_model cmo, car_country cc
+where ( cma.model = cmo.model and cc.maker ) = ( cma.maker and cc.maker ) = '현대';
+
+--차량 옵션에 'ABS'나 '물파리' 가 들어있는 차량의 제조사, 제조국, 모델명, 연식,
+-- 가격, 옵션, 입력일, 개월수, 이미지 조회. 단, 가격은 데이터가 존재하는 부분에 대해서만 3자리마다 ,출력
+-- 입력일은 년-월-일 형식, 개월수는 입력일로부터 현재까지 몇 개월 지났는지 출력(소수점x)
+-- 이미지는 확장자만 출력하고 배기량의 내림차순으로 정렬하되, 배기량이 같다면 가격의 오름차순.
+select cma.maker, cc.country, cma.model, cmo.car_year, cmo.cc, to_char(cmo.price, '999,999,999') price, cmo.car_option, to_char(cmo.hiredate, 'yy-mm-dd') hiredate, trunc((select months_between(sysdate, cmo.hiredate) from dual), 0) 개월수,
+		 substr(cmo.car_img, instr(car_img, '.')+1) img
+	from car_maker cma, car_model cmo, car_country cc
+	where ( cma.model = cmo.model and cc.maker = cma.maker ) and (cmo.car_option like '%ABS%' or cmo.car_option like '%물파리%')
+	order by cmo.cc desc, cmo.price;
+
+select * from zipcode;
+-- 부서번호, 부서명, 위치, 사원번호, 사원명, 연봉, 직무, 우편번호, 시도, 구군, 동, 번지를 조회
+-- 사원테이블의 사원번호와 우편번호 테이블의 seq를 조인조건으로 사용
+-- 단, 번지가 없다면 '번지없음' 출력
+select d.deptno, d.dname, d.loc, e.empno, e.ename, e.sal, e.job, z.zipcode, z.sido, z.gugun,
+			z.dong, nvl(z.bunji, '번지없음') bunji
+from dept d, emp e, zipcode z
+where e.deptno = d.deptno and d.deptno = z.seq;
